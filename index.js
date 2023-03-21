4201f7bb3891f4153de068307410ed44a35cd2d6
Romiszmyr <123408417+Romiszmyr@users.noreply.github.com>
2023-03-21 16:49:20 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ console.log ("Bienvenidos a mi Mundo");
 document.getElementById ("boton").addEventListener ("click", function() {
     console.log (Bienvenidos a mi Mundo desde Evenlistener);
 })
+
+<button onclick="href='https://www.google.com/maps/place/Gral.+Paz+441,+B7000+Tandil,+Provincia+de+Buenos+Aires/data=!4m2!3m1!1s0x95911f946e961489:0x36b886e86fec7402?sa=X&ved=2ahUKEwj2q72F4O39AhWpq5UCHRPgBCkQ8gF6BAgIEAI';">BIENVENIDOS A MI MUNDO</button>  
